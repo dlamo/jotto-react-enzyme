e5885f32f958eb2916d245ff1123ad1b58c0cc9a
David de Lamo <d.lamo.valverde@gmail.com>
2021-02-21 20:44:55 +0100

    App test fixed and add getLetterMatchCount helper

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,14 @@
 import App from './App';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr } from '../test/testUtils';
+
+const setup = (props = {}) => {
+  return shallow(<App {...props} />)
+}
 
 test('renders without error', () => {
-  render(<App />);
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-app');
+  expect(component.length).toBe(1);
 });
