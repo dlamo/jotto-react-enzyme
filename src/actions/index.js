8ed9f4956e5ec4d5a0c84440f693a899abdcfd90
David de Lamo <d.lamo.valverde@gmail.com>
2021-02-26 19:33:59 +0100

    Set up guessWords tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD',
 };
 
-export function correctGuess() {
-    return { type: actionTypes.CORRECT_GUESS };
-}
+// Returns Redux Thunk function that disàtches GUESS_WORD action and (conditionally) CORRECT_GUESS action
+export const guessWord = (guessedWord) => {
+    return function(dispatch, getState) {
+
+    };
+};
---
+++ src/integration.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+    describe('no guessed word', () => {
+        test('updates state correctly for unsuccessful guess', () => {
+
+        });
+        test('updates state correctly for successful guess', () => {
+
+        });
+    });
+    describe('some guessed word', () => {
+        test('updates state correctly for unsuccessful guess', () => {
+
+        });
+        test('updates state correctly for successful guess', () => {
+
+        });
+    });
+});
