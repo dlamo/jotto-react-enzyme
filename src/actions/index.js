defb44f4b7cb098a300259b7a7e22e01cb82bbb3
David de Lamo <d.lamo.valverde@gmail.com>
2021-02-27 12:55:24 +0100

    Add secretWord action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
+import axios from 'axios';
 import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
     GUESS_WORD: 'GUESS_WORD',
+    SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
 // Returns Redux Thunk function that disàtches GUESS_WORD action and (conditionally) CORRECT_GUESS action
 export const guessWord = (guessedWord) => {
-    return function(dispatch, getState) {
+    return (dispatch, getState) => {
         const secretWord = getState().secretWord;
         const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
@@ -26,3 +28,16 @@ export const guessWord = (guessedWord) => {
         };
     };
 };
+
+// http://localhost:3030 port where we could have a web server with an api to call
+export const getSecretWord = () => {
+    return (dispatch) => {
+        return axios.get('http://localhost:3030')
+            .then(response => {
+                dispatch({
+                    type: actionTypes.SET_SECRET_WORD,
+                    payload: response.data
+                });
+            });
+    }
+}
--- src/reducers/secretWordReducer.js
+++ src/reducers/secretWordReducer.js
@@ -1,3 +1,11 @@
+import { actionTypes } from "../actions";
+
 export default (state = null, action) => {
-    return state;
+    const { type, payload } = action;
+    switch(type) {
+        case actionTypes.SET_SECRET_WORD:
+            return payload;
+        default:
+            return state;
+    }
 };
